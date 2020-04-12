7dd0ab3cc7440ed516021fca16e9b830f7821995
chadwyck242 <chadwyck.codeart@gmail.com>
2020-04-12 14:27:03 -0600

    Upload most recently completed exercises

---
+++ Exercises/multiplication.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def main():
+    # Enter your solution here
+    for num in range(11):
+        print("4 multiplied by", num, "is", 4 * num)
+
+
+if __name__ == "__main__":
+    main()
